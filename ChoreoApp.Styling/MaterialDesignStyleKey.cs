c4aed1f9797172386a25f50984c07518e64b4de2
Johann Dirry <movgp0@gmail.com>
2025-12-13 16:35:37 +0100

    adding typography

---
+++ ChoreoApp.Styling/MaterialDesignStyleKey.cs
@@ -0,0 +1,42 @@
+namespace ChoreoApp.Styling;
+
+/// <summary>
+/// Resource keys for Material Design 3 text and control styles used in MAUI.
+/// </summary>
+public static class MaterialDesignStyleKey
+{
+    public const string MaterialDesignBodySmallTextBlock = nameof(MaterialDesignBodySmallTextBlock);
+    public const string MaterialDesignBodyMediumTextBlock = nameof(MaterialDesignBodyMediumTextBlock);
+    public const string MaterialDesignBodyLargeTextBlock = nameof(MaterialDesignBodyLargeTextBlock);
+
+    public const string MaterialDesignLabelSmallTextBlock = nameof(MaterialDesignLabelSmallTextBlock);
+    public const string MaterialDesignLabelMediumTextBlock = nameof(MaterialDesignLabelMediumTextBlock);
+    public const string MaterialDesignLabelLargeTextBlock = nameof(MaterialDesignLabelLargeTextBlock);
+
+    public const string MaterialDesignTitleSmallTextBlock = nameof(MaterialDesignTitleSmallTextBlock);
+    public const string MaterialDesignTitleMediumTextBlock = nameof(MaterialDesignTitleMediumTextBlock);
+    public const string MaterialDesignTitleLargeTextBlock = nameof(MaterialDesignTitleLargeTextBlock);
+
+    public const string MaterialDesignHeadlineSmallTextBlock = nameof(MaterialDesignHeadlineSmallTextBlock);
+    public const string MaterialDesignHeadlineMediumTextBlock = nameof(MaterialDesignHeadlineMediumTextBlock);
+    public const string MaterialDesignHeadlineLargeTextBlock = nameof(MaterialDesignHeadlineLargeTextBlock);
+
+    public const string MaterialDesignDisplaySmallTextBlock = nameof(MaterialDesignDisplaySmallTextBlock);
+    public const string MaterialDesignDisplayMediumTextBlock = nameof(MaterialDesignDisplayMediumTextBlock);
+    public const string MaterialDesignDisplayLargeTextBlock = nameof(MaterialDesignDisplayLargeTextBlock);
+
+    public const string MaterialDesignBody1Hyperlink = nameof(MaterialDesignBody1Hyperlink);
+    public const string MaterialDesignBody2Hyperlink = nameof(MaterialDesignBody2Hyperlink);
+    public const string MaterialDesignCaptionHyperlink = nameof(MaterialDesignCaptionHyperlink);
+    public const string MaterialDesignOverlineHyperlink = nameof(MaterialDesignOverlineHyperlink);
+    public const string MaterialDesignSubtitle1Hyperlink = nameof(MaterialDesignSubtitle1Hyperlink);
+    public const string MaterialDesignSubtitle2Hyperlink = nameof(MaterialDesignSubtitle2Hyperlink);
+    public const string MaterialDesignHeadline6Hyperlink = nameof(MaterialDesignHeadline6Hyperlink);
+    public const string MaterialDesignHeadline5Hyperlink = nameof(MaterialDesignHeadline5Hyperlink);
+    public const string MaterialDesignHeadline4Hyperlink = nameof(MaterialDesignHeadline4Hyperlink);
+    public const string MaterialDesignHeadline3Hyperlink = nameof(MaterialDesignHeadline3Hyperlink);
+    public const string MaterialDesignHeadline2Hyperlink = nameof(MaterialDesignHeadline2Hyperlink);
+    public const string MaterialDesignHeadline1Hyperlink = nameof(MaterialDesignHeadline1Hyperlink);
+
+    public const string MaterialDesign3ToggleSwitchStyle = nameof(MaterialDesign3ToggleSwitchStyle);
+}
