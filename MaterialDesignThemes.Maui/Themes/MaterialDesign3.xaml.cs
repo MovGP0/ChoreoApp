5f7e9bd6a93c06171438070ec54deb9502ceb41b
Johann Dirry <johann.dirry@microsea.at>
2025-12-26 19:54:32 +0100

    moving resources to design project

---
+++ MaterialDesignThemes.Maui/Themes/MaterialDesign3.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MaterialDesignThemes.Maui.Themes;
+
+public partial class MaterialDesign3 : ResourceDictionary
+{
+    public MaterialDesign3()
+    {
+        InitializeComponent();
+    }
+}
