e990f2304aa92d229ca0305d660590a6d8b7ac18
Johann Dirry <johann.dirry@microsea.at>
2025-12-26 18:02:52 +0100

    replacing shadow library and updating entry style

--- Maui.Shadows/AssemblyConfiguration.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://sharpnado.com", "Sharpnado.Shades")]
-[assembly: Microsoft.Maui.Controls.XmlnsPrefix("http://sharpnado.com", "sho")]
--- Maui.Shadows/Platforms/Android/JniExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace Sharpnado.Shades.Platforms.Android;
-
-internal static class JniExtensions
-{
-    public static bool IsNullOrDisposed(this Java.Lang.Object? javaObject)
-    {
-        return javaObject == null || javaObject.Handle == IntPtr.Zero;
-    }
-}
