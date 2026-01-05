fe337852cdd3bd2c1a72d782d25dd9b5d1aa5e5f
Johann Dirry <movgp0@gmail.com>
2026-01-05 18:18:29 +0100

    porting spelling keys

---
+++ MaterialDesignThemes.Maui/Spelling.cs
@@ -0,0 +1,9 @@
+namespace MaterialDesignThemes.Maui;
+
+public static class Spelling
+{
+    public const string SuggestionMenuItemStyleKey = "SpellingSuggestionMenuItemStyle";
+    public const string IgnoreAllMenuItemStyleKey = "SpellingIgnoreAllMenuItemStyle";
+    public const string NoSuggestionsMenuItemStyleKey = "SpellingNoSuggestionsMenuItemStyle";
+    public const string SeparatorStyleKey = "SpellingSeparatorStyle";
+}
