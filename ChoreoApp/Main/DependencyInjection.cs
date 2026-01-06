164f454cbdf13af3385343c79de62ccc1de221c4
Johann Dirry <movgp0@gmail.com>
2026-01-06 20:38:11 +0100

    removing duplicated save button

--- ChoreoApp/Main/DependencyInjection.cs
+++ ChoreoApp/Main/DependencyInjection.cs
@@ -8,7 +8,6 @@ public static class DependencyInjection
         services.AddTransient<MainPage>();
         services.AddTransient<MainViewModel>();
         services.AddTransient<IBehavior<MainViewModel>, Behaviors.ApplyInteractionModeBehavior>();
-        services.AddTransient<IBehavior<MainViewModel>, Behaviors.SaveChoreographyBehavior>();
         services.AddTransient<IBehavior<MainViewModel>, Behaviors.OpenSvgFileBehavior>();
         services.AddTransient<IBehavior<MainViewModel>, Behaviors.ShowDialogBehavior>();
         services.AddTransient<IBehavior<MainViewModel>, Behaviors.HideDialogBehavior>();
